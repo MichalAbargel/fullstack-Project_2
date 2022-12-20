7897ef86feb518c8dc68bfd6728feb1951038465
Michal Abargel <mbrgl715@gmail.com>
2022-12-20 23:28:32 +0200

    set home page, login and sign in

---
+++ js_files/index.js
@@ -0,0 +1,9 @@
+window.onload= function(){
+    check_local()
+}
+
+function check_local(){
+    if(localStorage["user_name"]){
+        document.querySelector("#user").innerHTML = `Hello ${localStorage["user_name"]}!`
+    }
+}
